bd64fc1be1d4500d9dc6433650ab441a09ae5b41
V ee <verinawasfy@gmail.com>
2021-07-10 08:30:40 +0200

    cutomer updates

--- Backend/Sales/Sales/Models/Customer.cs
+++ Backend/Sales/Sales/Models/Customer.cs
@@ -15,11 +15,6 @@ namespace Sales.Models
         [Required]
         [MinLength(3, ErrorMessage = "Name length can't be less than 3 letters.")]
         public string FName {set; get;}
-        //[EmailAddress]
-        //[RegularExpression(@"[a-z A-Z 0-9_]*@[a-z A-Z]+.[a-z A-Z]{2,4}", ErrorMessage = "Invalid Email.")]
-        //public string Email {get; set;}
-        //[DataType(DataType.Date)]
-        //public DateTime Date_Of_Birth {set; get;}
         [Phone]
         [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
         public int Phone_Num { set; get; }
